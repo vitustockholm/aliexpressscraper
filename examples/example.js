a41bd90392b38839942074fe023b4cc749a260a5
vitustockholm <vitustockholm@gmail.com>
2021-12-29 12:12:17 +0200

    aliexpress javascript product data scrasper

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,8 @@
-const scrape = require('./../index.js');
+const scrape = require('./../index');
 const product = scrape('32958933105');
 
-product.then(res => {
+product.then((res) => {
   console.log('The JSON: ', res);
+
+  document.body.innerHTML = '<h1>Data ' + res + '</h1>';
 });
